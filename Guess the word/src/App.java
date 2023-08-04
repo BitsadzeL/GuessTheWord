7c25b3676c25aac0621990bf12a15009d93885e8
Luka Bitsadze <bitsadzeluka@gmail.com>
2023-08-04 04:06:12 +0400

    Add files via upload

--- Guess the word/src/App.java
+++ Guess the word/src/App.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class App {
@@ -11,6 +12,7 @@ public class App {
         Scanner userInput =  new Scanner(System.in);
         Random rand= new Random();
         String[] AllWords=new String[3000]; //Because we have 3000 words in the file
+        ArrayList<Character> UsedLetters = new ArrayList<>();
         String RandomWordInStr;
         int RandomIndex=rand.nextInt(3001);
         int Attempts=5;
@@ -30,10 +32,13 @@ public class App {
         }
 
         System.out.println("I have chosen the random word and you have to guess it in 5 attempts.");
+        System.out.println("Enter just one letter on each turn and click enter to submit. Good luck!");
         System.out.print("Here is your word:");
         for(int i=0;i<CoveredRandomWord.length;i++){
             System.out.print(CoveredRandomWord[i]+" ");
         }        
+        System.out.println();
+        System.out.println("Attemps:"+Attempts);
         System.out.println();
 
 
@@ -54,15 +59,23 @@ public class App {
                     correctGuess = true; 
                 }
             }
-            if (correctGuess==false) {
-                Attempts--;
-                System.out.println("Wrong guess!");
+            if(correctGuess==false){
+                //System.out.println("Wrong guess!");
+                if(!CheckLetterRepeat(UsedLetters, user)){
+                    Attempts--;
+                    System.out.println("Wrong guess!");
+                }else{
+                    System.out.println("Wrong guess! but I will not take out your attempt, becaues you have tried this letter already");
+                    
+                }
+                UsedLetters.add(user);
             }else{
                 System.out.println("Correct!");
             }
 
 
             UpdateGuess(CoveredRandomWord, Attempts);
+            
             
             if(GameWin(CoveredRandomWord)==CoveredRandomWord.length){
                 System.out.println("YOU WON!");
@@ -78,13 +91,14 @@ public class App {
 
     }
 
+
+
     public static int GameWin(char[] a){
         int cnt=0;
         for(int i=0;i<a.length;i++){
             if(a[i]!='*'){
                 cnt++;
             }
-
         }
         return cnt;
     }
@@ -98,6 +112,15 @@ public class App {
         System.out.println();
         System.out.println();
         
+    }
+
+    public static boolean CheckLetterRepeat(ArrayList<Character> usedletters, char user){
+        for(int i=0;i<usedletters.size();i++){
+            if(usedletters.get(i).charValue()==user){
+                return true;
+            }
+        }
+        return false;
     }
 
 
